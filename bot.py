c3c226a78c25ed113d63758604c647dbd1b2b343
raphroll <raphaez@free.fr>
2026-08-01 00:11:23 +0200

    Fix comment percentage in bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ def generer_image(premiere_ligne, deuxieme_ligne):
 
     x_gauche = marge_gauche
     largeur_utile = largeur - marge_gauche - marge_droite
-    largeur_utile_premiere = largeur_utile * 0.6  # 70% de la largeur normale, pour forcer le retour à la ligne
+    largeur_utile_premiere = largeur_utile * 0.6  # % de la largeur normale, pour forcer le retour à la ligne
     y = marge_haut
 
     calque_texte = Image.new("RGBA", image.size, (255, 255, 255, 0))
