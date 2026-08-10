a78a599e6b724b48bcbc81d916867b24a618e1bc
raphroll <raphaez@free.fr>
2026-08-10 12:43:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ def decouper_selon_largeur(draw, texte, police, largeur_max):
 def calculer_taille_police_deuxieme(texte):
     """Réduit la taille de police si le texte est long, l'augmente s'il est court.
     Formule linéaire simple, bornée entre 55 (mini) et 80 (maxi)."""
-    taille = 100 - len(texte) * 0.5
+    taille = 96 - len(texte) * 0.5
     return max(60, min(90, taille))
 
 def generer_image(premiere_ligne, deuxieme_ligne):
@@ -101,7 +101,7 @@ def generer_image(premiere_ligne, deuxieme_ligne):
 
     x_gauche = marge_gauche
     largeur_utile = largeur - marge_gauche - marge_droite
-    largeur_utile_premiere = largeur_utile * 0.6  # % de la largeur normale, pour forcer le retour à la ligne
+    largeur_utile_premiere = largeur_utile * 0.75  # % de la largeur normale, pour forcer le retour à la ligne
     y = marge_haut
 
     calque_texte = Image.new("RGBA", image.size, (255, 255, 255, 0))
